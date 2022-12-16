daa9d9661cd265b3048d3ac33aa7ac9f9850bc36
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2022-12-16 03:12:45 +0000

    users page file created

---
+++ pages/users/index.js
@@ -0,0 +1,12 @@
+import Admin from "@/components/layouts/Admin"
+import React from 'react'
+
+const Users = () => {
+  return (
+    <Admin pageTitle="Usuarios" headerTitle="Usuarios">
+
+    </Admin>
+  )
+}
+
+export default Users
