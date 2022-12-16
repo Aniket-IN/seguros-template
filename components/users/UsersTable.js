a39ff46004245c92c91e5344557c72510a7e0ced
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2022-12-16 07:57:12 +0000

    unused code removed

--- components/users/UsersTable.js
+++ components/users/UsersTable.js
@@ -60,66 +60,6 @@ const UsersTable = () => {
         })}
       </Table.Tbody>
     </Table>
-
-    // <div className="overflow-x-auto">
-    //   <div className="inline-block min-w-full">
-    //     <div className="overflow-hidden">
-    //       <table className="min-w-full">
-    //         <thead className="bg-transparent">
-    //           <tr>
-    //             <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-    //               Correo
-    //             </th>
-    //             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-    //               Fecha de Creación
-    //             </th>
-    //             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-    //               Tipo
-    //             </th>
-    //             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-    //               Estado
-    //             </th>
-    //             <th scope="col" className="relative py-3.5 text-left pl-3 pr-4 sm:pr-6 text-sm font-semibold text-gray-900">
-    //               Acción
-    //             </th>
-    //           </tr>
-    //         </thead>
-    //         <tbody className="divide-y divide-gray-200 bg-white">
-    //           <tr key={'person.email'}>
-    //             <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-    //               <div className="flex items-center">
-    //                 <div className="h-10 w-10 flex-shrink-0">
-    //                   <img className="h-10 w-10 rounded-full" src={'person.image'} alt="" />
-    //                 </div>
-    //                 <div className="ml-4">
-    //                   <div className="font-medium text-gray-900">{'person.name'}</div>
-    //                   <div className="text-gray-500">{'person.email'}</div>
-    //                 </div>
-    //               </div>
-    //             </td>
-    //             <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-    //               <div className="text-gray-900">{'person.title'}</div>
-    //               <div className="text-gray-500">{'person.department'}</div>
-    //             </td>
-    //             <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-    //               <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-    //                 Active
-    //               </span>
-    //             </td>
-    //             <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{'person.role'}</td>
-    //             <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-    //               <a href="#" className="text-indigo-600 hover:text-indigo-900">
-    //                 Edit<span className="sr-only">, {'person.name'}</span>
-    //               </a>
-    //             </td>
-    //           </tr>
-    //         </tbody>
-    //       </table>
-    //     </div>
-    //   </div>
-    // </div>
-
-
   )
 }
 
