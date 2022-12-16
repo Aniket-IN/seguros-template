daff9ea245dbc6aee88d4a766693d1cd3710ea36
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2022-12-16 23:28:03 +0000

    password sent page added

---
+++ pages/password-sent/index.js
@@ -0,0 +1,39 @@
+import Auth from "@/components/layouts/Auth"
+import Link from "next/link"
+import React from 'react'
+
+const PasswordSent = () => {
+
+  const submit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <Auth>
+      <form onSubmit={submit} className="block w-full max-w-md bg-white shadow-md rounded border px-10 pt-14 pb-12">
+
+        {/* Back Btn */}
+        {/* <Link href="/login">
+          <ArrowLeftIcon className="w-6 h-6" />
+        </Link> */}
+
+        <h1 className="mt-3 text-2xl font-semibold">Enviamos la contraseña a tu correo</h1>
+        <p className="mt-4 text-secondary">No olvides de revisar tu carpeta de Correos no deseados o Spam.</p>
+
+
+        <Link href="/login" className="block text-center mt-44 py-5 px-4 w-full text-base text-white bg-primary rounded-xl focus:outline-none ring-primary focus:ring-2 focus:ring-offset-2">
+          Iniciar Sesión
+        </Link>
+
+        <div className="mt-9 flex justify-center">
+          <button type="button" className="inline-block mx-auto font-semibold underline hover:text-primary">
+            Volver a Iniciar Sesión
+          </button>
+        </div>
+
+      </form>
+    </Auth>
+  )
+}
+
+export default PasswordSent
