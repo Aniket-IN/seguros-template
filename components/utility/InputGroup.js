ebe904d00802222dd227f616fd855f642d475f88
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2022-12-16 18:54:48 +0000

    Input group added

---
+++ components/utility/InputGroup.js
@@ -0,0 +1,95 @@
+import { createContext, createElement, useContext } from "react";
+
+const InputGroupContext = createContext();
+
+const InputGroup = ({
+  as = "div",
+  isInvalid = false,
+  prepend = false,
+  append = false,
+  className = "",
+  ...props
+}) => {
+  return (
+    <InputGroupContext.Provider value={{ isInvalid, prepend, append }}>
+      {createElement(as, {
+        className: `w-full flex-row flex items-stretch ${className}`,
+        ...props,
+      })}
+    </InputGroupContext.Provider>
+  );
+};
+
+InputGroup.Prepend = ({ as = "div", className = "", ...props }) => {
+  return createElement(as, {
+    className: `
+      rounded-l overflow-hidden flex-grow flex-shrink-0 border-[#e2e8f0] border border-r-0 
+      ${className}
+    `,
+    ...props,
+  });
+};
+
+InputGroup.Append = ({ as = "div", className = "", ...props }) =>
+  createElement(as, {
+    className: `rounded-r overflow-hidden flex-grow flex-shrink-0 border-[#e2e8f0] border border-l-0 ${className}`,
+    ...props,
+  });
+
+InputGroup.Input = ({
+  as = "input",
+  className = "",
+  type = "text",
+  ...props
+}) => {
+  const { isInvalid, prepend, append } = useContext(InputGroupContext);
+
+  return createElement(as, {
+    className: `
+      ${isInvalid ? "border-red-600" : ""}
+      ${prepend ? "rounded-r" : ""}
+      ${append ? "rounded-l" : ""}
+      ${!append && !prepend ? "rounded" : ""} 
+      px-4 py-1.5 w-full border border-[#e2e8f0] 
+      outline-none focus:ring-1
+      focus:ring-primary focus:border-primary
+      ${className}`,
+    ...props,
+  });
+};
+
+InputGroup.Textarea = ({ as = "textarea", className = "", ...props }) => {
+  const { isInvalid, prepend, append } = useContext(InputGroupContext);
+  // transition ease-in-out duration-300
+  // focus:border-primary focus:border-2
+  return createElement(as, {
+    className: `
+      ${isInvalid ? "border-red-600" : ""}
+      ${prepend ? "rounded-r" : ""}
+      ${append ? "rounded-l" : ""}
+      ${!append && !prepend ? "rounded" : ""} 
+      px-4 py-2 w-full border border-[#e2e8f0] 
+      focus:outline-none focus:ring-1
+      focus:ring-primary focus:border-primary
+      ${className}`,
+    ...props,
+  });
+};
+
+InputGroup.Label = ({ as = "label", className = "", ...props }) =>
+  createElement(as, {
+    className: `flex mb-3 text-opacity-70 text-black text-sm cursor-pointer leading-none font-normal ${className}`,
+    ...props,
+  });
+
+InputGroup.Error = ({ as = "div", error = null, className = "", ...props }) =>
+  error
+    ? createElement(as, {
+      className: `text-sm text-red-600 mt-1 ${className}`,
+      children: error,
+      ...props,
+    })
+    : null;
+
+export { InputGroup };
+export default InputGroup;
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import Admin from "@/components/layouts/Admin"
 import UsersTable from "@/components/users/UsersTable"
+import { ChevronDownIcon } from "@heroicons/react/20/solid"
+import { DocumentTextIcon } from "@heroicons/react/24/outline"
+import InputGroup from "@/components/utility/InputGroup"
 
 const Users = () => {
   return (
     <Admin pageTitle="Usuarios" headerTitle="Usuarios">
       <div className="bg-neutral">
-        <div className="container-padding py-2.5 space-y-2 lg:space-y-0 lg:flex items-center justify-end gap-3">
+        <div className="container-padding py-2.5 space-y-2 lg:space-y-0 lg:flex items-center gap-3">
+          <div className="flex items-center gap-3">
+            <div>
+              <InputGroup>
+                <InputGroup.Input
+                  id="search"
+                  type="search"
+                  name="search"
+                />
+              </InputGroup>
+            </div>
 
+            <button
+              type="button"
+              className="self-stretch w-1/2 sm:w-auto sm:min-w-[150px] gap-2 inline-flex items-center px-2 sm:px-4 py-2 border border-transparent text-sm leading-4 font-normal rounded-md focus:outline-none bg-success text-white"
+            >
+              <DocumentTextIcon className="w-5 h-5" />
+              Descargar Excel
+            </button>
+          </div>
+          <div className="text-gray-900 text-sm text-right ml-auto">34 Usuarios</div>
         </div>
       </div>
 
