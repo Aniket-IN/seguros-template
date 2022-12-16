d032b4413b802905894e4e781e892be69540c6f4
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2022-12-16 20:00:08 +0000

    pagination added to users table

--- components/users/UsersTable.js
+++ components/users/UsersTable.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Table from "../Table"
+import { Menu, Transition } from "@headlessui/react"
+import classNames from "classnames"
+import { ChevronDownIcon } from "@heroicons/react/20/solid"
+import { FunnelIcon } from "@heroicons/react/24/outline"
 
 const UsersTable = () => {
   const headers = [
@@ -53,13 +57,67 @@ const UsersTable = () => {
                 </span>
               </Table.Td>
               <Table.Td>
-
+                <ActionBtn />
               </Table.Td>
             </Table.Tr>
           )
         })}
       </Table.Tbody>
     </Table>
+  )
+}
+
+const ActionBtn = () => {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex gap-2 items-center justify-center w-full rounded-md  px-4 py-2 bg-accent text-sm font-medium text-black hover:bg-gray-50 focus:outline-none focus:ring-2  focus:ring-primary">
+          Acción
+          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-left left-0 z-[1]  absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Lorem Ipsum
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Lorem Ipsum
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   )
 }
 
--- pages/users/index.js
+++ pages/users/index.js
@@ -2,16 +2,17 @@ import React, { Fragment } from 'react'
 import Admin from "@/components/layouts/Admin"
 import UsersTable from "@/components/users/UsersTable"
 import TopBar from "@/components/users/TopBar"
+import SamplePagination from "@/components/SamplePagination"
 
 const Users = () => {
   return (
     <Admin pageTitle="Usuarios" headerTitle="Usuarios">
-
+      
       <TopBar />
-
 
       <div className="container-padding">
         <UsersTable />
+        <SamplePagination />
       </div>
 
     </Admin>
