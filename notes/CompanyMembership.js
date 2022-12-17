e771d331daa0fd87c9588c74f65d2fc9b47fb1a1
Aniket-IN <aniket.das.in@gmail.com>
2022-12-18 04:42:30 +0530

    shields tab complete

--- notes/CompanyMembership.js
+++ notes/CompanyMembership.js
@@ -10,27 +10,22 @@ const Shields = () => {
         <Table>
           <Table.Thead>
             <Table.Tr>
-              <Table.Th>Escudo</Table.Th>
-              <Table.Th>Administrador</Table.Th>
-              <Table.Th>Fecha Creación</Table.Th>
-              <Table.Th>Usuario en escudo</Table.Th>
+              <Table.Th>Nombre</Table.Th>
+              <Table.Th>Fecha de Creación</Table.Th>
+              <Table.Th>Tipo</Table.Th>
               <Table.Th>Jerarquía</Table.Th>
-              <Table.Th>N° Miembros</Table.Th>
+              <Table.Th>Ubicaciones</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
             {[...Array(11)].map((item, index) => (
               <Table.Tr key={index}>
                 <Table.Td className="flex items-center gap-4">
-                  <img src="/assets/img/sample/companies/fanta.png" className="block w-11 aspect-square object-cover rounded-full" alt="" />
+                  <img src="/assets/img/sample/user-2.png" className="block w-11 aspect-square object-cover rounded-full" alt="" />
                   <div>
                     <p>Carlos Pérez Guerrero</p>
                     <p>UI123123</p>
                   </div>
-                </Table.Td>
-                <Table.Td>
-                  <p>Nombre de Admin</p>
-                  <p>ID 1231231</p>
                 </Table.Td>
                 <Table.Td>25/05/22</Table.Td>
                 <Table.Td>Estandar</Table.Td>
@@ -42,10 +37,9 @@ const Shields = () => {
                     Colaborativo
                   </span>
                 </Table.Td>
-                <Table.Td className="flex gap-5 items-center justify-center">
-                  <span>7</span>
-                  <button type="button" className="text-primary font-semibold hover:underline">
-                    Ver detalles
+                <Table.Td>
+                  <button className="text-primary font-semibold hover:underline">
+                    Historial de Ubicaciones
                   </button>
                 </Table.Td>
               </Table.Tr>
