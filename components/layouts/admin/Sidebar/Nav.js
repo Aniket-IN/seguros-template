5c810a947eb5e8d7d45ee84c0a8003d119d85cb6
Aniket-IN <aniket.das.in@gmail.com>
2022-12-19 16:48:17 +0530

    Nav links aded

--- components/layouts/admin/Sidebar/Nav.js
+++ components/layouts/admin/Sidebar/Nav.js
@@ -115,7 +115,7 @@ const Nav = () => {
     {
       id: 6,
       icon: (
-        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width={24} height={24} viewBox="0 0 24 24">
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
           <g transform="translate(18366 20330)">
             <path d="M14.182,15.273H12V9.818h2.182m0,9.818H12V17.455h2.182M1.091,22.909h24l-12-20.727Z" transform="translate(-18367.092 -20330.545)" fill="currentColor" />
           </g>
@@ -126,6 +126,80 @@ const Nav = () => {
       activePaths: [
         "/alerts-and-sos",
         "/alerts-and-sos/[alert_id]",
+      ],
+    },
+    {
+      id: 7,
+      icon: (
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
+          <g transform="translate(18366 20270)">
+            <path d="M24,21.714v1.143H3.429V21.714l2.286-2.286V12.571A7.989,7.989,0,0,1,11.429,4.9V4.571a2.286,2.286,0,1,1,4.571,0V4.9a7.989,7.989,0,0,1,5.714,7.669v6.857L24,21.714M16,24a2.286,2.286,0,0,1-4.571,0" transform="translate(-18367.715 -20272.285)" fill="currentColor" />
+          </g>
+        </svg>
+      ),
+      title: "Enviar notificaciones",
+      href: "#",
+      activePaths: [],
+    },
+    {
+      id: 8,
+      icon: (
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
+          <g transform="translate(18366 20211)" >
+            <path d="M19.091,18.349l2.662,1.538-.818,1.418L17.455,19.3V15.273h1.636v3.076m7.091.2a7.636,7.636,0,0,1-15.273,0A7.237,7.237,0,0,1,11,17.455H2.182V4.364H21.818v7.287a7.647,7.647,0,0,1,4.364,6.895M11.651,15.273a8.874,8.874,0,0,1,.655-1.124.939.939,0,0,1-.305.033,3.273,3.273,0,1,1,3.273-3.273,3.188,3.188,0,0,1-.109.8,7.676,7.676,0,0,1,3.382-.8A7.237,7.237,0,0,1,19.636,11V8.727a2.181,2.181,0,0,1-2.182-2.182H6.545A2.174,2.174,0,0,1,4.364,8.727v4.364a2.181,2.181,0,0,1,2.182,2.182h5.105M24,18.545A5.455,5.455,0,1,0,18.545,24,5.457,5.457,0,0,0,24,18.545Z" transform="translate(-18368.182 -20214.273)" fill="currentColor" />
+          </g>
+        </svg>
+      ),
+      title: "Pagos Membresías",
+      href: "/payment-memberships",
+      activePaths: [
+        "/payment-memberships",
+        "/payment-memberships/[payment_membership_id]",
+      ],
+    },
+    {
+      id: 9,
+      icon: (
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
+          <g transform="translate(18366 20151)">
+            <path d="M12,4.8H4.8A2.392,2.392,0,0,0,2.4,7.2V21.6A2.4,2.4,0,0,0,4.8,24H24a2.4,2.4,0,0,0,2.4-2.4V9.6A2.4,2.4,0,0,0,24,7.2H14.4Z" transform="translate(-18368.4 -20153.4)" fill="currentColor" />
+          </g>
+        </svg>
+      ),
+      title: "Documentación",
+      href: "/documentation",
+      activePaths: [
+        "/documentation",
+      ],
+    },
+    {
+      id: 10,
+      icon: (
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
+          <g transform="translate(18366 20075)">
+            <path d="M13.091,1.091a9.777,9.777,0,0,0-9.818,9.818v7.636a3.273,3.273,0,0,0,3.273,3.273H9.818V13.091H5.455V10.909a7.636,7.636,0,0,1,15.273,0v2.182H16.364v8.727h4.364v1.091H13.091v2.182h6.545a3.273,3.273,0,0,0,3.273-3.273V10.909A9.8,9.8,0,0,0,13.091,1.091Z" transform="translate(-18367.092 -20076.092)" fill="currentColor" />
+          </g>
+        </svg>
+      ),
+      title: "Soporte",
+      href: "/support",
+      activePaths: [
+        "/support",
+      ],
+    },
+    {
+      id: 11,
+      icon: (
+        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
+          <g transform="translate(18366 20031)">
+            <path d="M21.6,22.8H7.2V21.12c0-2.4,4.8-3.72,7.2-3.72s7.2,1.32,7.2,3.72M14.4,8.4A3.6,3.6,0,1,1,10.8,12a3.6,3.6,0,0,1,3.6-3.6m0-4.8a1.2,1.2,0,1,1-1.2,1.2,1.2,1.2,0,0,1,1.2-1.2m8.4,0H17.784a3.586,3.586,0,0,0-6.768,0H6A2.4,2.4,0,0,0,3.6,6V22.8A2.4,2.4,0,0,0,6,25.2H22.8a2.4,2.4,0,0,0,2.4-2.4V6A2.4,2.4,0,0,0,22.8,3.6Z" transform="translate(-18368.4 -20032.199)" fill="currentColor" />
+          </g>
+        </svg>
+      ),
+      title: "Roles",
+      href: "/roles",
+      activePaths: [
+        "/roles",
       ],
     },
   ]
