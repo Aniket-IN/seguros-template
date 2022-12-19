a68289323e10439a4a5f4479384fd523abbb9821
Aniket-IN <aniket.das.in@gmail.com>
2022-12-19 08:14:34 +0530

    chat tab file created

---
+++ pages/shields/[shield_id]/chat/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ShieldLayout from "@/components/layouts/ShieldLayout"
+import Table from "@/components/Table"
+import SamplePagination from "@/components/SamplePagination"
+
+export default function index() {
+  return (
+    <ShieldLayout pageTitle="Escudos" headerTitle="Escudos">
+      
+    </ShieldLayout>
+  )
+}
+
