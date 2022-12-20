7871da46f65c31edbcf4c3ee8f3ba85854edf12f
Aniket-IN <aniket.das.in@gmail.com>
2022-12-20 13:24:43 +0530

    faqs and tickets page file created

--- pages/documentation/faqs/index.js
+++ pages/documentation/faqs/index.js
@@ -1,11 +1,11 @@
 import DocumentationLayout from "@/components/layouts/DocumentationLayout"
 import React from 'react'
 
-export default function index() {
+export default function faqs() {
   return (
     <DocumentationLayout>
       {/* TODO: Design this page
-      XD: https://xd.adobe.com/view/258a5967-33a7-4223-b884-a052f322a683-70d9/screen/29e8daea-cfa4-48c1-9cbc-8c41f5905db6/specs/
+      XD: https://xd.adobe.com/view/258a5967-33a7-4223-b884-a052f322a683-70d9/screen/3b9ebd56-387b-4da9-bc68-301e1a8faba6/specs/
       */}
     </DocumentationLayout>
   )
