7c3f0b0ac1aa93b902790a8b70ef05a057fa4273
Aniket-IN <aniket.das.in@gmail.com>
2022-12-20 13:15:08 +0530

    Roles page complete

---
+++ components/roles/UserRoleCard.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import SectionHeading from "../SectionHeading"
+import { PencilIcon } from "@heroicons/react/24/solid"
+
+const UserRoleCard = () => {
+  const data = [
+    {
+      key: "Nombre completo",
+      value: "Mario Lopez",
+    },
+    {
+      key: "N°. de Identificación",
+      value: "9871454196",
+    },
+    {
+      key: "Correo electrónico",
+      value: "ejemplo@gmail.com",
+    },
+    {
+      key: "País",
+      value: "Ecuador",
+    },
+    {
+      key: "Teléfono",
+      value: "(+57) 1234 567 890",
+    },
+  ]
+
+  return (
+    <div className="p-5 bg-white pb-14">
+
+      <div className="flex flex-col gap-6">
+
+        <div className="flex gap-5">
+          <div className="flex items-center justify-center">
+            <div className="flex-shrink-0 w-28 h-28">
+              <img className="block w-28 h-28 object-cover rounded-full" src="/assets/img/sample/user-1.png" alt="User" />
+            </div>
+          </div>
+          <div className="space-y-2.5">
+            <h3 className="text-xl font-semibold">Lucas Rodriguez</h3>
+            <span className="inline-flex items-center px-3 py-1.5 rounded-full text-sm font-semibold bg-primary bg-opacity-20 text-primary">
+              <svg className="mr-1.5 h-2 w-2" fill="currentColor" viewBox="0 0 8 8">
+                <circle cx={5} cy={4} r={3} />
+              </svg>
+              Administrador
+            </span>
+            <div className="flex gap-5">
+              <div className="rounded border px-4 py-2.5">
+                <dd className="text-xs text-secondary">Escudos de Empresa</dd>
+                <dd className="text-xl">124</dd>
+              </div>
+              <div className="rounded border px-4 py-2.5">
+                <dd className="text-xs text-secondary">Miembros creados</dd>
+                <dd className="text-xl">120</dd>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex justify-between items-center">
+            <SectionHeading>Datos de Administrador</SectionHeading>
+            <button className="bg-accent rounded-full px-4 py-2 font-medium inline-flex justify-center items-center gap-2">
+              <PencilIcon className="w-4 h-4" />
+              <span>Editar</span>
+            </button>
+          </div>
+          <hr className="my-2" />
+
+          <div className="grid grid-cols-2 gap-4 text-sm mt-4">
+            {data.map((item) => (
+              <>
+                <dd className="font-semibold">{item.key}</dd>
+                <dd>{item.value}</dd>
+              </>
+            ))}
+          </div>
+
+          <div className="flex justify-between items-center mt-6">
+            <SectionHeading>Contraseña</SectionHeading>
+            <button className="bg-accent rounded-full px-4 py-2 font-medium inline-flex justify-center items-center gap-2">
+              <PencilIcon className="w-4 h-4" />
+              <span>Editar</span>
+            </button>
+          </div>
+          <hr className="my-2" />
+
+
+          <div className="grid grid-cols-2 gap-4 text-sm mt-4">
+            <dd className="text-secondary">Contraseña</dd>
+            <dd>***********</dd>
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default UserRoleCard
---
+++ pages/roles/index.js
@@ -0,0 +1,123 @@
+import React, { Fragment } from 'react'
+import Admin from "@/components/layouts/Admin"
+import UserRoleCard from "@/components/roles/UserRoleCard"
+import SectionHeading from "@/components/SectionHeading"
+import Table from "@/components/Table"
+import { Menu, Transition } from "@headlessui/react"
+import classNames from "classnames"
+import { ChevronDownIcon } from "@heroicons/react/20/solid"
+import Link from "next/link"
+
+
+function index() {
+  return (
+    <Admin>
+      <div className="container-padding py-7 flex flex-col lg:flex-row gap-5">
+
+        <div className="w-full lg:max-w-lg">
+          <UserRoleCard />
+        </div>
+
+        <div className="flex-grow space-y-5">
+          <div className="p-5 bg-white space-y-5">
+            <div className="flex flex-col md:flex-row gap-4 justify-between">
+              <SectionHeading>Roles de Administradores</SectionHeading>
+              <button className="px-4 py-2 bg-primary text-white rounded text-sm ml-auto">+ Crear Admin</button>
+            </div>
+            <Table wrapperClassName="bg-accent px-3 flex-grow overflow-auto max-h-[1024px]" className="relative">
+              <Table.Thead className="bg-accent sticky top-0 z-[10]">
+                <Table.Tr>
+                  <Table.Th>Usuario</Table.Th>
+                  <Table.Th>Tipo</Table.Th>
+                  <Table.Th>Fecha Creación</Table.Th>
+                  <Table.Th>Ult. Actividad</Table.Th>
+                  <Table.Th>Acciones</Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                {[...Array(20)].map((item, index) => (
+                  <Table.Tr key={index}>
+                    <Table.Td>
+                      <div className="flex items-center gap-3">
+                        <img src="/assets/img/sample/user-2.png" className="block w-11 aspect-square object-cover" />
+                        <div>
+                          <dd className="text-base">Mario Lopez</dd>
+                          <dd className="text-sm">ID-UI123123</dd>
+                        </div>
+                      </div>
+                    </Table.Td>
+                    <Table.Td>Superadmin</Table.Td>
+                    <Table.Td>23/09/2022</Table.Td>
+                    <Table.Td>23/09/2022, 18:00 hrs</Table.Td>
+                    <Table.Td>
+                      <ActionBtn />
+                    </Table.Td>
+                  </Table.Tr>
+                ))}
+              </Table.Tbody>
+            </Table>
+          </div>
+
+        </div>
+      </div>
+    </Admin>
+  )
+}
+
+
+const ActionBtn = () => {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex gap-2 items-center justify-center w-full rounded-md  px-4 py-2 bg-accent text-sm font-medium text-black hover:bg-gray-50 focus:outline-none focus:ring-2  focus:ring-primary">
+          Acción
+          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-right right-0 z-[1]  absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Ver detalles
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Suspender cuenta
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
+
+
+export default index
