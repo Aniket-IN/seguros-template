e526eea95ebd640d2545de6a771694f7f36ef1f6
Aniket-IN <aniket.das.in@gmail.com>
2022-12-22 23:03:17 +0530

    `UserCountChart.js` aded

--- components/home/charts/UserCountChart.js
+++ components/home/charts/UserCountChart.js
@@ -1,9 +1,81 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  interaction: {
+    intersect: false,
+  },
+  scales: {
+    x: {
+      border: {
+        display: false,
+      },
+      ticks: {
+        font: {
+          size: 14,
+          weight: 700,
+          color: 'black',
+        },
+      },
+      grid: {
+        display: false,
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      display: true,
+    },
+    title: {
+      display: false,
+    },
+  },
+};
+
+
+const labels = ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 250 })),
+      borderColor: '#1555ED',
+      backgroundColor: '#fff',
+      pointRadius: 3,
+      pointHoverRadius: 4,
+      pointBorderWidth: 3,
+    },
+  ],
+};
 
 const UserCountChart = () => {
   return (
     <div>
-      
+      <Line options={options} data={data} />
     </div>
   )
 }
