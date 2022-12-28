ee13e1526672a54aa1f982d1ad494303a33210aa
Aniket-IN <aniket.das.in@gmail.com>
2022-12-28 19:57:39 +0530

    both side same height

--- pages/shields/[shield_id]/route-history/index.js
+++ pages/shields/[shield_id]/route-history/index.js
@@ -14,8 +14,10 @@ export default function index() {
       XD Link: https://xd.adobe.com/view/258a5967-33a7-4223-b884-a052f322a683-70d9/screen/cf3baed5-8183-4510-8037-d7828fe0d793/specs/
       */}
       <div className="flex flex-col xl:flex-row gap-5 mt-4">
-        <div className="xl:max-w-md w-full">
-          <Table wrapperClassName=" max-h-[750px] no-scrollbar" className="relative">
+
+
+        <div className="xl:max-w-md w-full h-[800px]">
+          <Table wrapperClassName="h-full no-scrollbar" className="relative">
             <Table.Thead className="sticky top-0 bg-accent">
               <Table.Tr>
                 <Table.Th>Nombre</Table.Th>
@@ -60,56 +62,54 @@ export default function index() {
           </Table>
         </div>
 
-        <div className="flex-grow">
-          <div className="bg-white p-5 space-y-3">
-            <SectionHeading>Historial de Rutas</SectionHeading>
-            <div className="flex items-center gap-4 justify-end">
-              <div className="flex items-center gap-2 justify-end text-sm">
-                <span>Buscar</span>
-                <div>
-                  <InputGroup>
-                    <InputGroup.Input
-                      type="date"
-                      className="bg-accent"
-                    />
-                  </InputGroup>
-                </div>
-                <button className="self-stretch px-3 font-medium bg-primary text-white rounded focus:ring-2 ring-offset-2">
-                  Buscar
-                </button>
+        <div className="flex-grow flex flex-col h-[800px] bg-white p-5 space-y-3">
+          <SectionHeading>Historial de Rutas</SectionHeading>
+          <div className="flex items-center gap-4 justify-end">
+            <div className="flex items-center gap-2 justify-end text-sm">
+              <span>Buscar</span>
+              <div>
+                <InputGroup>
+                  <InputGroup.Input
+                    type="date"
+                    className="bg-accent"
+                  />
+                </InputGroup>
               </div>
-              <DownloadRoutesBtn className="bg-accent px-4 py-2.5 inline-flex gap-2.5 text-sm rounded border">
-                <ArrowDownTrayIcon className="w-4 h-4" />
-                <span className="font-medium">Descargar Rutas</span>
-              </DownloadRoutesBtn>
+              <button className="self-stretch px-3 font-medium bg-primary text-white rounded focus:ring-2 ring-offset-2">
+                Buscar
+              </button>
             </div>
-            <div className="px-4 pb-4 bg-accent">
-              <Table wrapperClassName="max-h-[750px] overflow-auto" className="relative">
-                <Table.Thead className="sticky top-0 bg-accent">
-                  <Table.Tr>
-                    <Table.Th>ID Ruta</Table.Th>
-                    <Table.Th>Velocidad</Table.Th>
-                    <Table.Th>Detalles de Ruta</Table.Th>
+            <DownloadRoutesBtn className="bg-accent px-4 py-2.5 inline-flex gap-2.5 text-sm rounded border">
+              <ArrowDownTrayIcon className="w-4 h-4" />
+              <span className="font-medium">Descargar Rutas</span>
+            </DownloadRoutesBtn>
+          </div>
+          <div className="relative bg-accent flex-grow">
+            <Table wrapperClassName="absolute inset-0 w-full h-full px-4 pb-4 overflow-auto" className="relative">
+              <Table.Thead className="sticky top-0 bg-accent">
+                <Table.Tr>
+                  <Table.Th>ID Ruta</Table.Th>
+                  <Table.Th>Velocidad</Table.Th>
+                  <Table.Th>Detalles de Ruta</Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                {[...Array(20)].map((item, index) => (
+                  <Table.Tr key={index}>
+                    <Table.Td className="font-semibold">Ruta #E12341RF212</Table.Td>
+                    <Table.Td>
+                      <dd>10:00 hrs - 19:20 hrs</dd>
+                      <dd>11/03/2022</dd>
+                    </Table.Td>
+                    <Table.Td>
+                      <RouteDetailsModalBtn className="text-primary hover:underline font-semibold">
+                        Ver Detalles
+                      </RouteDetailsModalBtn>
+                    </Table.Td>
                   </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>
-                  {[...Array(20)].map((item, index) => (
-                    <Table.Tr key={index}>
-                      <Table.Td className="font-semibold">Ruta #E12341RF212</Table.Td>
-                      <Table.Td>
-                        <dd>10:00 hrs - 19:20 hrs</dd>
-                        <dd>11/03/2022</dd>
-                      </Table.Td>
-                      <Table.Td>
-                        <RouteDetailsModalBtn className="text-primary hover:underline font-semibold">
-                          Ver Detalles
-                        </RouteDetailsModalBtn>
-                      </Table.Td>
-                    </Table.Tr>
-                  ))}
-                </Table.Tbody>
-              </Table>
-            </div>
+                ))}
+              </Table.Tbody>
+            </Table>
           </div>
         </div>
 
