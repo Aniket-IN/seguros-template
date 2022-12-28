4119f00eb9213297a7f2f98a91c8a7e29ac73450
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2022-12-28 19:01:14 +0000

    Action button added to TabNavs

--- components/layouts/user/UserTabNav.js
+++ components/layouts/user/UserTabNav.js
@@ -3,7 +3,8 @@ import { ChevronDownIcon } from "@heroicons/react/20/solid"
 import classNames from "classnames"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Menu, Transition } from "@headlessui/react"
 
 const UserTabNav = () => {
 
@@ -75,15 +76,17 @@ const UserTabNav = () => {
       </div>
 
       <div className="text-right ml-auto">
-        <button className="inline-flex items-center justify-center text-sm">
+        {/* <button className="inline-flex items-center justify-center text-sm">
           <span>Acción</span>
           <ChevronDownIcon className="w-5 h-5" />
-        </button>
+        </button> */}
+        <ActionBtn />
       </div>
 
     </nav>
   )
 }
+
 
 const Item = ({ tab }) => {
   const router = useRouter();
@@ -105,6 +108,71 @@ const Item = ({ tab }) => {
         {tab.title}
       </Link>
     </li>
+  )
+}
+
+const ActionBtn = () => {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      {({ open }) => (
+        <>
+          <Menu.Button
+            className={classNames(
+              "w-full capitalize gap-2 inline-flex justify-between items-center px-2 sm:px-4 py-2 border border-transparent text-sm leading-4 font-normal rounded-md focus:outline-none",
+              open ? "text-white bg-primary" : "text-black",
+            )}
+          >
+            Acción
+            <ChevronDownIcon
+              className={classNames("-mr-1 ml-2 h-5 w-5 duration-300 transition-transform", open ? 'rotate-180' : '')}
+              aria-hidden="true"
+            />
+          </Menu.Button>
+
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute origin-top-left left-0 lg:left-auto lg:origin-top-right lg:right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm capitalize'
+                      )}
+                    >
+                      Ver detalles
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm capitalize'
+                      )}
+                    >
+                      Suspender cuenta
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
   )
 }
 
