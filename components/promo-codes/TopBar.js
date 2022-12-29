a5482ac2d04cbaf76751eebbe2e41b4e37a89b2b
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2022-12-29 09:38:11 +0000

    Promocode filter-dropdown added

--- components/promo-codes/TopBar.js
+++ components/promo-codes/TopBar.js
@@ -7,6 +7,7 @@ import { FunnelIcon } from "@heroicons/react/24/outline"
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid"
 import PromoCodeFormModal from "./PromoCodeFormModal"
 import ConfirmationModal from "../utility/ConfirmationModal"
+import FilterDropDownBtn from "../utility/FilterDropDownBtn"
 
 
 const TopBar = () => {
@@ -31,56 +32,52 @@ const TopBar = () => {
         <div className="flex items-center gap-3 flex-grow">
 
 
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-              <Menu.Button className="inline-flex gap-2 items-center justify-center w-full rounded-md  px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2  focus:ring-primary">
-                <FunnelIcon className="w-5 h-5" />
-                Filtros
-                <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-              </Menu.Button>
-            </div>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="origin-top-left left-0   absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Lorem Ipsum
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Lorem Ipsum
-                      </a>
-                    )}
-                  </Menu.Item>
-                </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
+          <FilterDropDownBtn.Primary
+            groups={[
+              {
+                id: 1,
+                title: "Membership",
+                options: [
+                  {
+                    id: 1,
+                    label: 'Nivel 1',
+                    value: 1,
+                  },
+                  {
+                    id: 2,
+                    label: 'Nivel 2',
+                    value: 2,
+                  },
+                  {
+                    id: 3,
+                    label: 'Nivel 3',
+                    value: 3,
+                  },
+                ],
+              },
+              {
+                id: 2,
+                title: "Status",
+                options: [
+                  {
+                    id: 1,
+                    label: 'Active',
+                    value: 'Active',
+                  },
+                  {
+                    id: 2,
+                    label: 'Inactive',
+                    value: 'Inactive',
+                  },
+                  {
+                    id: 3,
+                    label: 'Suspended',
+                    value: 'Suspended',
+                  },
+                ],
+              },
+            ]}
+          />
 
 
           {/* <div className="text-gray-900 text-sm text-right ml-auto">34 Usuarios</div> */}
