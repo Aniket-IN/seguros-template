84b83c8259b4314432eb2046d2d4b2130cdbd68e
Aniket-IN <aniket.das.in@gmail.com>
2023-01-02 13:56:26 +0530

    `Unauthorized` error handler added

--- hooks/useAxios.js
+++ hooks/useAxios.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useLocalStorage } from "react-use";
+import { useRouter } from "next/router";
 
 
 const useAxios = () => {
-  const [token, setToken, remove] = useLocalStorage('access_token', null)
+  const router = useRouter()
+  const [token, setToken, removeToken] = useLocalStorage('access_token', null)
+  const [isLoggedIn, setIsLoggedIn, removeIsloggedIn] = useLocalStorage('is_logged_in', null)
 
+
+  // Create axios instance
   const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
@@ -14,6 +19,41 @@ const useAxios = () => {
     },
     withCredentials: true,
   })
+
+
+  // Add a request interceptor
+  axios.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+
+  // Add a response interceptor
+  instance.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      const isUnauthorized = [401].includes(error?.response?.status);
+      if (isUnauthorized) {
+        removeToken();
+        removeIsloggedIn();
+        router.push('/')
+      }
+
+      return Promise.reject(error);
+    },
+  );
 
   return { axios: instance }
 }
