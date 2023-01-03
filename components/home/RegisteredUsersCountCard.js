0f79b2d901d691e0d4ebd7b4db71f2812c5edfa1
Aniket-IN <64463116+Aniket-IN@users.noreply.github.com>
2023-01-03 18:01:17 +0000

    registed users count data update

--- components/home/RegisteredUsersCountCard.js
+++ components/home/RegisteredUsersCountCard.js
@@ -8,36 +8,57 @@ const RegisteredUsersCountCard = ({ selectedMonth }) => {
 
   const { axios } = useAxios()
 
-  const fetchData = () => {
+  const fetchData = ({ month, year }) => {
     return axios.get('/api/dashboard/registered-users/', {
       params: {
-        month: selectedMonth.value,
+        month,
+        year,
       },
     })
   };
 
-  const { isLoading, data: response, isError, error } = useQuery(['registered-users-count', selectedMonth.value], fetchData, {
-    refetchOnWindowFocus: false
+  const now = new Date();
+
+  function padWithZero(num, targetLength) {
+    return String(num).padStart(targetLength, '0');
+  }
+
+  const selectedDate = new Date(`${now.getFullYear()}-${padWithZero(selectedMonth.value, 2)}-15T14:10:28.570073Z`)
+
+  const previousDate = new Date(selectedDate.setMonth(selectedDate.getMonth() - 1));
+  const currentDate = new Date(`${now.getFullYear()}-${padWithZero(selectedMonth.value, 2)}-15T14:10:28.570073Z`)
+
+  const currentMonthQuery = useQuery([`registered-users-count-this-month-${selectedMonth.value}`],
+    () => fetchData({ month: currentDate.getMonth() + 1, year: currentDate.getFullYear() }), {
+    refetchOnWindowFocus: false,
   })
 
+  const prevMonthQuery = useQuery([`registered-users-count-prev-month-${selectedMonth.value}`],
+    () => fetchData({ month: previousDate.getMonth() + 1, year: previousDate.getFullYear() }), {
+    refetchOnWindowFocus: false,
+  })
+
+  const currentMonthData = currentMonthQuery?.data?.data?.data ?? {}
+  const prevMonthData = prevMonthQuery?.data?.data?.data ?? {}
+
   const data = {
-    total_users: 2750,
-    last_month: 2500,
+    total_users: currentMonthData.total_users,
+    last_month: prevMonthData.total_users,
     items: [
       {
         id: 1,
         title: "Particulares",
-        count: 100,
+        count: currentMonthData.individuals_users,
       },
       {
         id: 2,
         title: "Empresas",
-        count: 100,
+        count: currentMonthData.companies,
       },
       {
         id: 3,
         title: "Suspendidos",
-        count: 75,
+        count: 0,
       },
     ],
   }
@@ -48,10 +69,14 @@ const RegisteredUsersCountCard = ({ selectedMonth }) => {
         <h3 className="font-semibold text-sm mb-2">Usuarios registrados</h3>
 
         <div className="flex justify-between items-center">
-          <span className="text-5xl">2750</span>
+          <span className="text-5xl">
+            {data.total_users}
+          </span>
           <div className="text-right space-y-1">
             <dd className="text-secondary text-xs">Mes pasado</dd>
-            <dd className="text-2xl">2500</dd>
+            <dd className="text-2xl">
+              {data.last_month}
+            </dd>
           </div>
         </div>
 
