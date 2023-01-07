6656475168009526b4aea12dc3b56ce59ca5236f
Aniket-IN <aniket.das.in@gmail.com>
2023-01-07 22:22:34 +0530

    membership done

--- pages/shields/[shield_id]/membership/index.js
+++ pages/shields/[shield_id]/membership/index.js
@@ -3,12 +3,52 @@ import ShieldLayout from "@/components/layouts/ShieldLayout";
 import Table from "@/components/Table";
 import SamplePagination from "@/components/SamplePagination";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import useTableData from "@/hooks/useTableData";
+import Pagination from "@/components/Pagination";
+import { format } from "date-fns";
+
+
+const pageSize = 1
 
 export default function index() {
+  const router = useRouter();
+  const { shield_id } = router.query;
+
+  const {
+    search,
+    setSearch,
+    currentTableData,
+    tempFilters,
+    setTempFilters,
+    applyFilters,
+    isLoading,
+    isError,
+    error,
+    sort,
+    setSort,
+    allData,
+    currentPage,
+    setCurrentPage,
+    isSuccess,
+    resetPage
+  } = useTableData({
+    dataUrl: `/api/shield/sheild-membership/?id=${shield_id}`,
+    pageSize: pageSize,
+    queryKeys: [`shield-${shield_id}-members-table-data`],
+    enabled: !!shield_id,
+  })
+
+
   return (
     <ShieldLayout pageTitle="Escudos" headerTitle="Escudos">
       <div className="mt-5">
-        <Table>
+        <Table
+          dataCount={currentTableData.length}
+          isLoading={isLoading}
+          isError={isError}
+          error={error}
+        >
           <Table.Thead>
             <Table.Tr>
               <Table.Th>Membresía</Table.Th>
@@ -20,16 +60,22 @@ export default function index() {
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
-            {[...Array(2)].map((item, index) => (
-              <Table.Tr key={index}>
-                <Table.Td className="font-semibold">Nivel 0</Table.Td>
-                <Table.Td>22/08/2022</Table.Td>
-                <Table.Td>23/09/2022</Table.Td>
-                <Table.Td>Pago#123123</Table.Td>
-                <Table.Td>T-1231231231</Table.Td>
+            {isSuccess && currentTableData?.map((membership) => (
+              <Table.Tr key={membership.id}>
+                <Table.Td className="font-semibold">{membership.membership}</Table.Td>
+                <Table.Td>{format(new Date(membership.date), 'dd/MM/yyyy')}</Table.Td>
+                <Table.Td>
+                  {membership.membership_end_date ? format(new Date(membership.membership_end_date), 'dd/MM/yyyy') : null}
+                </Table.Td>
+                <Table.Td>
+                  Pago#{membership.unitary_amount}
+                </Table.Td>
+                <Table.Td>
+                  {membership.transaction_id}
+                </Table.Td>
                 <Table.Td>
                   <Link
-                    href="/payment-memberships/1"
+                    href={`/payment-memberships/${membership.id}`}
                     className="font-semibold text-primary hover:underline"
                   >
                     Ver detalles
@@ -39,7 +85,16 @@ export default function index() {
             ))}
           </Table.Tbody>
         </Table>
-        <SamplePagination />
+        {/* <SamplePagination /> */}
+        {isSuccess && (
+          <Pagination
+            className="mt-3.5"
+            totalCount={allData.length}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            onPageChange={setCurrentPage}
+          />
+        )}
       </div>
     </ShieldLayout>
   );
