7a9bd7afcc8c8c8f9f69fcd9f10fdbda807973ef
Aniket-IN <aniket.das.in@gmail.com>
2023-01-07 17:12:41 +0530

    shield details added

--- components/layouts/shield/ShieldCard.js
+++ components/layouts/shield/ShieldCard.js
@@ -1,5 +1,11 @@
+import Badge from "@/components/Badge";
+import ProfilePicture from "@/components/ProfilePicture";
+import useAxios from "@/hooks/useAxios";
 import classNames from "classnames";
+import { format } from "date-fns";
+import { useRouter } from "next/router";
 import React, { createElement } from "react";
+import { useQuery } from "react-query";
 
 const CardWrapper = ({ as = 'div', className = '', ...props }) => {
   return createElement(as, {
@@ -9,28 +15,43 @@ const CardWrapper = ({ as = 'div', className = '', ...props }) => {
 }
 
 const ShieldCard = () => {
+  const { axios } = useAxios()
+  const router = useRouter();
+
+  const { shield_id } = router.query;
+
+  const fetchData = () => {
+    return axios.get(`/api/shield/get-single/${shield_id}`);
+  }
+
+  // React-query for data fetching
+  const { isLoading, isError, refetch, isRefetching, isSuccess, data: response, error } = useQuery(
+    `shield-${shield_id}-details`,
+    fetchData,
+    {
+      refetchOnWindowFocus: false,
+      enabled: !!shield_id
+    }
+  );
+
+  const shield = response?.data?.data ?? {}
+
   const data = [
     {
       key: "Administradores",
-      value: (
-        <>
-          Juan Jesús Ledesma
-          <br />
-          Lucia Candiotti
-        </>
-      ),
+      value: shield.admin,
     },
     {
       key: "N° de Miembros",
-      value: "08",
+      value: shield.members_count,
     },
     {
       key: "Fecha de creación",
-      value: "25/05/22",
+      value: shield.created_at ? format(new Date(shield.created_at), 'dd/MM/yy') : '',
     },
     {
       key: "Tipo de Usuario",
-      value: "Corporativo",
+      value: shield.shield_type,
     },
   ];
 
@@ -38,23 +59,20 @@ const ShieldCard = () => {
     return (
       <CardWrapper>
         <div className="text-center">
-          <img
+          <ProfilePicture
             className="mx-auto aspect-square w-24 rounded-full"
-            src="/assets/img/sample/companies/fanta.png"
-            alt="Fanta"
+            src={shield.logo}
+            alt={shield.shield_name}
           />
-          <h4 className="mt-3 text-lg font-semibold">Familia</h4>
-          <p className="text-secondary">ID UI123123</p>
-          <span className="mt-1.5 inline-flex items-center rounded-full bg-green-100 px-3 py-1.5 text-sm font-semibold text-green-600">
-            <svg
-              className="mr-1.5 h-2 w-2 text-green-600"
-              fill="currentColor"
-              viewBox="0 0 8 8"
-            >
-              <circle cx={5} cy={4} r={3} />
-            </svg>
-            Activo
-          </span>
+          <h4 className="mt-3 text-lg font-semibold capitalize">{shield.shield_name}</h4>
+          <p className="text-secondary">ID {shield.id}</p>
+          <div className="mt-1.5">
+            {shield.condition ? (
+              <Badge.Md className="text-green-600 bg-green-100" text="Activo" />
+            ) : (
+              <Badge.Md className="text-green-600 bg-green-100" text="Inactive" />
+            )}
+          </div>
         </div>
 
         <div className="flex-grow">
