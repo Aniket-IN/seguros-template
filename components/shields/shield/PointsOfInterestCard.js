a8ebc1204f2eec895257d70699bb89b95f00ce86
Aniket-IN <aniket.das.in@gmail.com>
2023-01-07 16:41:25 +0530

    poi update

--- components/shields/shield/PointsOfInterestCard.js
+++ components/shields/shield/PointsOfInterestCard.js
@@ -1,33 +1,6 @@
-import useAxios from "@/hooks/useAxios";
-import { useRouter } from "next/router";
 import React from "react";
-import { useQuery } from "react-query";
 
-const PointsOfInterestCard = () => {
-  const { axios } = useAxios()
-  const router = useRouter();
-
-  const { shield_id } = router.query;
-
-  const fetchData = () => {
-    return axios.get("/api/shield/shield-point-of-interest/", {
-      params: {
-        id: shield_id
-      }
-    });
-  }
-
-  // React-query for data fetching
-  const { isLoading, isError, refetch, isRefetching, isSuccess, data: response, error } = useQuery(
-    `shield-${shield_id}-point-of-interests`,
-    fetchData,
-    {
-      refetchOnWindowFocus: false,
-      enabled: !!shield_id
-    }
-  );
-
-  const items = response?.data ?? []
+const PointsOfInterestCard = ({ items }) => {
 
   return (
     <div className="flex h-[800px] flex-col bg-white">
@@ -36,7 +9,7 @@ const PointsOfInterestCard = () => {
       <div className="flex flex-grow flex-col gap-3.5 p-5">
         <h3 className="text-lg font-medium">Puntos de Interés</h3>
         <ul className="h-0 flex-grow space-y-1.5 overflow-auto bg-accent p-3">
-          {items.map((item, index) => (
+          {items.map((item) => (
             <li key={item.id} className="bg-white px-4 py-3">
               <h5 className="text-sm">Punto {item.id}</h5>
               <p className="text-xs text-secondary">
--- pages/shields/[shield_id]/index.js
+++ pages/shields/[shield_id]/index.js
@@ -2,14 +2,42 @@ import React from "react";
 import ShieldLayout from "@/components/layouts/ShieldLayout";
 import PointsOfInterestCard from "@/components/shields/shield/PointsOfInterestCard";
 import PointHistoryCard from "@/components/shields/shield/PointHistoryCard";
+import useAxios from "@/hooks/useAxios";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+
 
 export default function index() {
-  
+
+  const { axios } = useAxios()
+  const router = useRouter();
+
+  const { shield_id } = router.query;
+
+  const fetchData = () => {
+    return axios.get("/api/shield/shield-point-of-interest/", {
+      params: {
+        id: shield_id
+      }
+    });
+  }
+
+  // React-query for data fetching
+  const { isLoading, isError, refetch, isRefetching, isSuccess, data: response, error } = useQuery(
+    `shield-${shield_id}-point-of-interests`,
+    fetchData,
+    {
+      refetchOnWindowFocus: false,
+      enabled: !!shield_id
+    }
+  );
+
+  const pois = response?.data ?? []
 
   return (
     <ShieldLayout pageTitle="Escudos" headerTitle="Escudos">
       <div className="mt-4 grid grid-cols-1 gap-5 lg:grid-cols-2">
-        <PointsOfInterestCard />
+        <PointsOfInterestCard items={pois} />
         <PointHistoryCard />
       </div>
     </ShieldLayout>
