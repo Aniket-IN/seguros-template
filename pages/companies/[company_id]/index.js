b240b2cc937d55b0abaf83631db17d8b28829ab8
Aniket-IN <aniket.das.in@gmail.com>
2023-01-08 18:16:46 +0530

    company/shield page

--- pages/companies/[company_id]/index.js
+++ pages/companies/[company_id]/index.js
@@ -3,12 +3,51 @@ import Table from "@/components/Table";
 import SamplePagination from "@/components/SamplePagination";
 import CompanyLayout from "@/components/layouts/CompanyLayout";
 import Link from "next/link";
+import useTableData from "@/hooks/useTableData";
+import { useRouter } from "next/router";
+import { format } from "date-fns";
+import Pagination from "@/components/Pagination";
+
+const pageSize = 1
 
 export default function index() {
+  const router = useRouter();
+  const { company_id } = router.query;
+
+  const {
+    search,
+    setSearch,
+    currentTableData,
+    tempFilters,
+    setTempFilters,
+    applyFilters,
+    isLoading,
+    isError,
+    error,
+    sort,
+    setSort,
+    allData,
+    currentPage,
+    setCurrentPage,
+    isSuccess,
+    resetPage
+  } = useTableData({
+    dataUrl: `/api/company/company-shields/?id=${company_id}`,
+    pageSize: pageSize,
+    queryKeys: [`company-${company_id}-shields-table-data`],
+    enabled: !!company_id,
+  })
+
+
   return (
     <CompanyLayout pageTitle="Empresas" headerTitle="Empresas">
       <div className="mt-5">
-        <Table>
+        <Table
+          dataCount={currentTableData.length}
+          isLoading={isLoading}
+          isError={isError}
+          error={error}
+        >
           <Table.Thead>
             <Table.Tr>
               <Table.Th>Nombre</Table.Th>
@@ -20,25 +59,30 @@ export default function index() {
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
-            {[...Array(11)].map((item, index) => (
-              <Table.Tr key={index}>
+            {isSuccess && currentTableData.map((shield, index) => (
+              <Table.Tr key={shield.id}>
                 <Table.Td>
                   <div className="flex min-w-fit items-center gap-4">
-                    <div className="flex aspect-square w-11 items-center justify-center rounded-full bg-warning text-2xl text-white">
-                      T
-                    </div>
+                    <img
+                      src={process.env.NEXT_PUBLIC_BACKEND_URL + shield.logo}
+                      className="aspect-square w-11 rounded-full object-cover"
+                    />
                     <div>
-                      <p className="font-semibold">Nombre de Escudo</p>
-                      <p>ESC123123</p>
+                      <p className="font-semibold capitalize">{shield.shield_name}</p>
+                      <p>{shield.shield_code}</p>
                     </div>
                   </div>
                 </Table.Td>
                 <Table.Td>8</Table.Td>
-                <Table.Td>23/09/2022</Table.Td>
-                <Table.Td>Nivel 1</Table.Td>
+                <Table.Td>
+                  {format(new Date(shield.created_at), 'dd/MM/yyyy')}
+                </Table.Td>
+                <Table.Td>
+                  {/* Nivel 1 */}
+                </Table.Td>
                 <Table.Td className="text-secondary">
-                  <p>Alexander Quiroz López</p>
-                  <p>ID UI 2457896</p>
+                  <p className="capitalize">{shield.admin?.full_name}</p>
+                  <p>ID {shield.admin?.id}</p>
                 </Table.Td>
                 <Table.Td>
                   <Link
@@ -52,7 +96,15 @@ export default function index() {
             ))}
           </Table.Tbody>
         </Table>
-        <SamplePagination />
+        {isSuccess && (
+          <Pagination
+            className="mt-3.5"
+            totalCount={allData.length}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            onPageChange={setCurrentPage}
+          />
+        )}
       </div>
     </CompanyLayout>
   );
