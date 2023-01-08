fe1fdc548cc5ecf820f97efc612dfced50c1db5d
Aniket-IN <aniket.das.in@gmail.com>
2023-01-08 16:01:01 +0530

    company layout dynamic

--- components/layouts/CompanyLayout.js
+++ components/layouts/CompanyLayout.js
@@ -4,8 +4,34 @@ import Admin from "@/components/layouts/Admin";
 import { ChevronLeftIcon } from "@heroicons/react/20/solid";
 import CompanyTabNav from "./company/CompanyTabNav";
 import CompanyCard from "./company/CompanyCard";
+import useAxios from "@/hooks/useAxios";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+
 
 const CompanyLayout = ({ children, pageTitle = null, headerTitle = "" }) => {
+  const { axios } = useAxios()
+  const router = useRouter();
+
+  const { company_id } = router.query;
+
+  const fetchData = () => {
+    return axios.get(`/api/company/company-single/${company_id}`);
+  }
+
+  // React-query for data fetching
+  const { isLoading, isError, refetch, isRefetching, isSuccess, data: response, error } = useQuery(
+    `company-${company_id}-details`,
+    fetchData,
+    {
+      refetchOnWindowFocus: false,
+      enabled: !!company_id
+    }
+  );
+
+  const company = response?.data?.data ?? {}
+
+
   return (
     <Admin pageTitle={pageTitle} headerTitle={headerTitle}>
       <section className="container-padding space-y-5">
@@ -17,13 +43,13 @@ const CompanyLayout = ({ children, pageTitle = null, headerTitle = "" }) => {
           <span>Volver</span>
         </Link>
 
-        <h2 className="text-2xl font-medium">Carlos Pérez</h2>
+        <h2 className="text-2xl font-medium capitalize">{company.name}</h2>
       </section>
 
       <section className="container-padding mt-6">
         <div className="gap-5 2xl:flex">
           <div className="w-full flex-shrink-0 2xl:max-w-xs">
-            <CompanyCard />
+            <CompanyCard {...{ company, isSuccess }} />
           </div>
 
           <div className="flex-grow">
--- components/layouts/company/CompanyCard.js
+++ components/layouts/company/CompanyCard.js
@@ -1,70 +1,74 @@
+import Badge from "@/components/Badge";
+import { format } from "date-fns";
 import React from "react";
 
-const CompanyCard = () => {
+const CompanyCard = ({ company = {}, isSuccess = false }) => {
+
   const data = [
     {
       key: "Nº de escudos",
-      value: "10",
+      value: company.shields,
     },
     {
       key: "Asesor comercial",
-      value: "Juan Jesús Ledesma",
+      value: company?.super_admin?.userprofile?.full_name,
     },
     {
       key: "ID Asesor",
-      value: "AC 7845216",
+      value: company?.super_admin?.userprofile?.id,
     },
     {
       key: "Teléfono asesor",
-      value: "+52 68457213",
+      value: company?.super_admin?.userprofile?.phone,
     },
     {
       key: "Fecha de creación",
-      value: "25/05/22",
+      value: company.created_at ? format(new Date(company.created_at), 'dd/MM/yy') : '',
     },
     {
       key: "Tipo de usuario",
-      value: "Corporativo",
+      value: company?.super_admin?.userprofile?.user_type,
     },
   ];
 
-  return (
-    <div className="items-center gap-7 bg-white px-5 py-10 sm:flex 2xl:block">
-      <div className="text-center">
-        <img
-          className="mx-auto aspect-square w-24 rounded-full"
-          src="/assets/img/sample/companies/fanta.png"
-          alt="Fanta"
-        />
-        <h4 className="mt-3 text-lg font-semibold">Fanta</h4>
-        <p className="text-secondary">EMP UI123123</p>
-        <span className="mt-1.5 inline-flex items-center rounded-full bg-green-100 px-3 py-1.5 text-sm font-semibold text-green-600">
-          <svg
-            className="mr-1.5 h-2 w-2 text-green-600"
-            fill="currentColor"
-            viewBox="0 0 8 8"
-          >
-            <circle cx={5} cy={4} r={3} />
-          </svg>
-          Activo
-        </span>
-      </div>
+  if (isSuccess) {
+    return (
+      <div className="items-center gap-7 bg-white px-5 py-10 sm:flex 2xl:block">
+        <div className="text-center">
+          <img
+            className="mx-auto aspect-square w-24 rounded-full object-cover"
+            src={company.image ? `${process.env.NEXT_PUBLIC_BACKEND_URL}${company.image}` : null}
+          />
+          <h4 className="mt-3 text-lg font-semibold capitalize">{company.name}</h4>
+          <p className="text-secondary">{company.company_code}</p>
+          <div className="mt-1.5">
+            {!!company.suspended ? (
+              <Badge.Md text="Suspended" className="text-warning bg-warning bg-opacity-20" />
+            ) : (
+              <Badge.Md text="Activo" className="text-success bg-green-100" />
+            )}
+          </div>
+        </div>
 
-      <div className="flex-grow">
-        <h5 className="border-b pb-2 text-lg font-semibold">Detalles</h5>
-        <div className="mt-4 grid grid-cols-1 gap-y-6 gap-x-5 text-sm sm:grid-cols-2 2xl:grid-cols-1">
-          {data.map((item) => {
-            return (
-              <dl key={item.key}>
-                <dd className="font-semibold">{item.key}</dd>
-                <dd className="text-secondary">{item.value}</dd>
-              </dl>
-            );
-          })}
+        <div className="flex-grow">
+          <h5 className="border-b pb-2 text-lg font-semibold">Detalles</h5>
+          <div className="mt-4 grid grid-cols-1 gap-y-6 gap-x-5 text-sm sm:grid-cols-2 2xl:grid-cols-1">
+            {data.map((item) => {
+              return (
+                <dl key={item.key}>
+                  <dd className="font-semibold">{item.key}</dd>
+                  <dd className="text-secondary">{item.value}</dd>
+                </dl>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div className="items-center gap-7 bg-white px-5 py-10 sm:flex 2xl:block" />
+
 };
 
 export default CompanyCard;
