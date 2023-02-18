b9b7b013dbd71d32fae13f56abdb575decf64f60
Aniket-IN <aniket.das.in@gmail.com>
2023-02-18 23:07:40 +0530

    company complete

--- pages/companies/[company_id]/promo-codes/index.js
+++ pages/companies/[company_id]/promo-codes/index.js
@@ -1,41 +1,53 @@
 import React from "react";
 import Table from "@/components/Table";
-import SamplePagination from "@/components/SamplePagination";
 import CompanyLayout from "@/components/layouts/CompanyLayout";
 import useTableData from "@/hooks/useTableData";
+import { useRouter } from "next/router";
+import Pagination from "@/components/Pagination";
+import classNames from "classnames";
+
+const pageSize = 1;
 
 export default function PromoCodes() {
-  // const {
-  //   search,
-  //   setSearch,
-  //   currentTableData,
-  //   tempFilters,
-  //   setTempFilters,
-  //   applyFilters,
-  //   isLoading,
-  //   isError,
-  //   error,
-  //   sort,
-  //   setSort,
-  //   allData,
-  //   currentPage,
-  //   setCurrentPage,
-  //   isSuccess,
-  //   resetPage
-  // } = useTableData({
-  //   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  //   noAuth: true,
-  //   dataCallback: (data) => data?.data?.data,
-  //   dataUrl: `/api/company/company-members/?id=${company_id}`,
-  //   pageSize: pageSize,
-  //   queryKeys: [`company-${company_id}-members-table-data`],
-  //   enabled: !!company_id,
-  // })
+  const router = useRouter();
+  const { company_id } = router.query;
+
+  const {
+    search,
+    setSearch,
+    currentTableData,
+    tempFilters,
+    setTempFilters,
+    applyFilters,
+    isLoading,
+    isError,
+    error,
+    sort,
+    setSort,
+    allData,
+    currentPage,
+    setCurrentPage,
+    isSuccess,
+    resetPage,
+  } = useTableData({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    noAuth: true,
+    dataCallback: (data) => data?.data,
+    dataUrl: `/api/admin/promo-code/?company_id=${company_id}`,
+    pageSize: pageSize,
+    queryKeys: [`promocodes`, { company_id }],
+    enabled: !!company_id,
+  });
 
   return (
     <CompanyLayout pageTitle="Empresas" headerTitle="Empresas">
       <div className="mt-5">
-        <Table>
+        <Table
+          dataCount={currentTableData.length}
+          isLoading={isLoading}
+          isError={isError}
+          error={error}
+        >
           <Table.Thead>
             <Table.Tr>
               <Table.Th>ID de código</Table.Th>
@@ -48,31 +60,50 @@ export default function PromoCodes() {
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
-            {[...Array(1)].map((item, index) => (
-              <Table.Tr key={index}>
-                <Table.Td>XYZ123123</Table.Td>
-                <Table.Td>FANTA2000</Table.Td>
-                <Table.Td>Nivel 1</Table.Td>
-                <Table.Td>20%</Table.Td>
-                <Table.Td>125</Table.Td>
-                <Table.Td>Código para Fanta - Lorem Ipsum</Table.Td>
-                <Table.Td>
-                  <span className="inline-flex items-center rounded-full bg-green-100 px-3 py-1.5 text-sm font-semibold text-green-600">
-                    <svg
-                      className="mr-1.5 h-2 w-2 text-green-600"
-                      fill="currentColor"
-                      viewBox="0 0 8 8"
+            {isSuccess &&
+              currentTableData?.map((promocode, index) => (
+                <Table.Tr key={promocode.id}>
+                  <Table.Td>{promocode.code_id}</Table.Td>
+                  <Table.Td>{promocode.promo_code}</Table.Td>
+                  <Table.Td>{promocode.membership}</Table.Td>
+                  <Table.Td>{promocode.discount}%</Table.Td>
+                  <Table.Td>{promocode.stocks}</Table.Td>
+                  <Table.Td>{promocode.label}</Table.Td>
+                  <Table.Td>
+                    <span
+                      className={classNames(
+                        "inline-flex items-center rounded-full  px-3 py-1.5 text-sm font-semibold",
+                        promocode.state
+                          ? "bg-green-100 text-green-600"
+                          : "bg-red-100 text-red-600"
+                      )}
                     >
-                      <circle cx={5} cy={4} r={3} />
-                    </svg>
-                    Activo
-                  </span>
-                </Table.Td>
-              </Table.Tr>
-            ))}
+                      <svg
+                        className={classNames(
+                          "mr-1.5 h-2 w-2",
+                          promocode.state ? "text-green-600" : "text-red-600"
+                        )}
+                        fill="currentColor"
+                        viewBox="0 0 8 8"
+                      >
+                        <circle cx={5} cy={4} r={3} />
+                      </svg>
+                      {promocode.state ? "Activo" : "Inactivo"}
+                    </span>
+                  </Table.Td>
+                </Table.Tr>
+              ))}
           </Table.Tbody>
         </Table>
-        {/* <SamplePagination /> */}
+        {isSuccess && (
+          <Pagination
+            className="mt-3.5"
+            totalCount={allData.length}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            onPageChange={setCurrentPage}
+          />
+        )}
       </div>
     </CompanyLayout>
   );
